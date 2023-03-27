071a38990b213cb304ee73d11ba559d18da35473
abinrushud <abinrushud@tahakom.com>
2023-03-27 12:31:55 +0300

    add loading indeicator

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -8,17 +8,23 @@ export async function handleSubmit(event) {
 	let city = document.getElementById('city').value;
 	let startDate = document.getElementById('start_date').value;
 	let endDate = document.getElementById('end_date').value;
+	const isLoading = document.getElementsByClassName('loader')[0];
+
 
 	const diff = dayjs(endDate).diff(dayjs(startDate),'d');
-
+  isLoading.style.display = 'block';
 	const { destination, image, app_max_temp, app_min_temp } = await getData(city);
+  isLoading.style.display = 'none';
 	updateUI(destination, image, app_max_temp, app_min_temp, diff);
 }
 
 export async function getData(city) {
 	const { data: location } = await axios.get('http://localhost:3000/location', { params: { city } });
+	console.log(location);
 	const { data: weather } = await axios.get('http://localhost:3000/weather', { params: location });
+	console.log(weather);
 	const { data: images } = await axios.get('http://localhost:3000/image', { params: { city } });
+	console.log(images);
 
 	const { app_max_temp, app_min_temp } = weather.data[0];
 	const destination = weather.city_name + ', ' + weather.country_code;
